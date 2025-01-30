c7a3eb6516744fa0d3523448b27473840968ffcd
Dharani <rs.dharani04122005@gmail.com>
2025-01-31 00:28:03 +0530

    deployment changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000,  // Set a specific port
+    host: true,   // Allows access from any host (important for deployment)
+  },
+  preview: {
+    port: 10000,  // Ensures `vite preview` runs on the same port
+    host: true,
+  },
+  build: {
+    outDir: "dist",  // Ensure output directory is correct
+  }
 })
